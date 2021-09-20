c1d2bd9ec7902b20ab3fd6320ed9e61818d319db
Justin Park <jmpark@email.wm.edu>
2021-09-20 17:34:48 -0400

    Starts with two randomly placed numbers of either 2 or 4

--- Game2048ByJustinPark/src/edu/wm/cs/cs301/game2048/State.java
+++ Game2048ByJustinPark/src/edu/wm/cs/cs301/game2048/State.java
@@ -1,43 +1,102 @@
 package edu.wm.cs.cs301.game2048;
+import java.util.Random;
 
 public class State implements GameState {
-
-	public State(GameState original) {
-		// TODO Auto-generated constructor stub
+	
+	int[][] board = new int[4][4];
+	
+	public State() {
+		for(int x=0; x<4; x++) {
+			for(int y=0; y<4; y++) {
+				setValue(x, y, 0);
+			}
+		}
 	}
-
-	public void State1(GameState original) {
-		// TODO Auto-generated constructor stub
+	
+	public State(GameState original) {
+		if(original == null) {
+			for(int x=0; x<4; x++) {
+				for(int y=0; y<4; y++) {
+					setValue(x, y, 0);
+				}
+			}
+		}
+		else {
+			for(int x=0; x<4; x++) {
+				for(int y=0; y<4; y++) {
+					setValue(x, y, original.getValue(x, y));
+				}
+			}
+		}
 	}
 
 	@Override
 	public int getValue(int xCoordinate, int yCoordinate) {
-		// TODO Auto-generated method stub
-		return 0;
+		return board[xCoordinate][yCoordinate];
 	}
 
 	@Override
 	public void setValue(int xCoordinate, int yCoordinate, int value) {
-		// TODO Auto-generated method stub
-
+		board[xCoordinate][yCoordinate] = value;
 	}
 
 	@Override
 	public void setEmptyBoard() {
-		// TODO Auto-generated method stub
-
+		for(int x=0; x<4; x++) {
+			for(int y=0; y<4; y++) {
+				board[x][y] = 0;
+			}
+		}
 	}
 
 	@Override
 	public boolean addTile() {
-		// TODO Auto-generated method stub
-		return false;
+		int count = 0;
+		for(int x=0; x<4; x++) {
+			for(int y=0; y<4; y++) {
+				if(board[x][y] == 0) {
+					count++;
+				}
+			}
+		}
+		if(count == 0) {
+			return false;
+		}
+		Random rand = new Random();
+		int index = rand.nextInt(count);
+		int tileNum = rand.nextInt(2);
+		if(tileNum == 0) {
+			tileNum = 2;
+		}
+		else {
+			tileNum = 4;
+		}
+		for(int x=0; x<4; x++) {
+			for(int y=0; y<4; y++) {
+				if(board[x][y] == 0) {
+					if(index == 0) {
+						board[x][y] = tileNum;
+						index--;
+					}
+					else {
+						index--;
+					}
+				}
+			}
+		}
+		return true;
 	}
 
 	@Override
 	public boolean isFull() {
-		// TODO Auto-generated method stub
-		return false;
+		for(int x=0; x<4; x++) {
+			for(int y=0; y<4; y++) {
+				if(board[x][y] == 0) {
+					return false;
+				}
+			}
+		}
+		return true;
 	}
 
 	@Override
